53f32ba8e145ba5b48c0a9a69fadfe3d7c50d0df
jfrausto7 <jacob_frausto@brown.edu>
2020-12-15 21:38:14 -0700

    learned about console.log, arrays, and objects

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,45 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserInput() {
   return parseInt(userInput.value);
 }
 
-function createAndWriteOutput(operator, resultBefore, calcNumber){
+function createAndWriteOutput(operator, resultBefore, calcNumber) {
   const description = `${resultBefore} ${operator} ${calcNumber}`;
   outputResult(currentResult, description);
-
 }
 
+/*
+ * Four functions for each button!
+ */
 function add() {
   const enteredNum = getUserInput();
-  const initial = currentResult
+  const initial = currentResult;
   currentResult = currentResult + enteredNum;
-  createAndWriteOutput('+', initial, enteredNum);
+  createAndWriteOutput("+", initial, enteredNum);
+  const logEntry = {
+    operation: "ADD",
+    prevResult: initial,
+    number: enteredNum,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
-function subtract(){
+function subtract() {
   const enteredNum = getUserInput();
-  const initial = currentResult
+  const initial = currentResult;
   currentResult = currentResult - enteredNum;
-  createAndWriteOutput('-', initial, enteredNum);
+  createAndWriteOutput("-", initial, enteredNum);
 }
 
-function multiply(){
+function multiply() {
   const enteredNum = getUserInput();
-  const initial = currentResult
+  const initial = currentResult;
   currentResult = currentResult * enteredNum;
-  createAndWriteOutput('*', initial, enteredNum);
+  createAndWriteOutput("*", initial, enteredNum);
 }
 
-function divide(){
+function divide() {
   const enteredNum = getUserInput();
-  const initial = currentResult
+  const initial = currentResult;
   currentResult = currentResult / enteredNum;
-  createAndWriteOutput('/', initial, enteredNum);
+  createAndWriteOutput("/", initial, enteredNum);
 }
 
-
+//event listeners!
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
 multiplyBtn.addEventListener("click", multiply);
