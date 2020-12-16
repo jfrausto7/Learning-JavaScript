d81446619c3c4f470b1760afac6c31f7fccc8c84
jfrausto7 <jacob_frausto@brown.edu>
2020-12-15 19:05:03 -0700

    added functionality for other buttons

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, "");
+function getUserInput() {
+  return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(operator, resultBefore, calcNumber){
+  const description = `${resultBefore} ${operator} ${calcNumber}`;
+  outputResult(currentResult, description);
+
+}
+
+function add() {
+  const enteredNum = getUserInput();
+  const initial = currentResult
+  currentResult = currentResult + enteredNum;
+  createAndWriteOutput('+', initial, enteredNum);
+}
+
+function subtract(){
+  const enteredNum = getUserInput();
+  const initial = currentResult
+  currentResult = currentResult - enteredNum;
+  createAndWriteOutput('-', initial, enteredNum);
+}
+
+function multiply(){
+  const enteredNum = getUserInput();
+  const initial = currentResult
+  currentResult = currentResult * enteredNum;
+  createAndWriteOutput('*', initial, enteredNum);
+}
+
+function divide(){
+  const enteredNum = getUserInput();
+  const initial = currentResult
+  currentResult = currentResult / enteredNum;
+  createAndWriteOutput('/', initial, enteredNum);
+}
+
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
