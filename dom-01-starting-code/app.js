3e5fd4b8c6ae31c1c44198aaee6ab7d5022cd0a1
jfrausto7 <jacob_frausto@brown.edu>
2020-12-21 21:16:28 -0700

    learned about inserting, removing, and styling nodes in the DOM

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -15,3 +15,29 @@ const listItemElements = document.getElementsByTagName("li"); //live list of ite
 for (const listItemEl of listItemElements) {
   console.dir(listItemEl);
 }
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+console.log(button);
+
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+  // if(section.className === 'red-bg visible'){
+  //   section.className = 'red-bg invisible';
+  // }else{
+  //   section.className = 'red-bg visible';
+  // }
+
+  // section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
+
+
+//add element
+const newLi = document.createElement('li');
+newLi.textContent = 'new shtuff';
+const list = document.querySelector('ul:first-of-type');
+list.appendChild(newLi);
